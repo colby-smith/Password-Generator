9f80417da36ffb4b54deae4a5fc1e10786fa0f37
Colby <colbysmith99@hotmail.com>
2024-03-11 14:23:27 +0000

    Added define_special_characters function

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ import string
     # allow the user to accept this password as the one they'd like to use or let them keep generating till they're happy
 
 def main():
-    print ("------------------------Password Generator------------------------")
+    print ("--------------------------------Password Generator--------------------------------")
     define_password_length()
+    print ("----------------------------------------------------------------------------------")
+    define_special_characters()
+    print ("----------------------------------------------------------------------------------")
 
 def define_password_length():
-    min_password_length = int(input("Please eneter the minimum amount of characters for your password as numbers: "))
-    max_password_length = int(input("Please eneter the maximum amount of characters for your password as numbers: "))
+    min_password_length = int(input("Please eneter the minimum amount of characters for your password, as a number: "))
+    max_password_length = int(input("Please eneter the maximum amount of characters for your password, as a number: "))
+    print ("----------------------------------------------------------------------------------")
+    print (f"Your password will be generated between {min_password_length} and {max_password_length} characters.")
     if min_password_length > 50:
         raise Exception ("Minimum character length cannot be greater than 50")
     if min_password_length <= 0:
@@ -21,6 +26,16 @@ def define_password_length():
     if max_password_length <= min_password_length:
         raise Exception ("Maximum password length cannot be less than minimum")
     else:
-        print (min_password_length, max_password_length)
+        return (min_password_length, max_password_length)
+    
+def define_special_characters():
+    special_characters = (input("Would you like your password to contain special characters, yes or no: ")).lower()
+    if special_characters == "yes":
+        print ("----------------------------------------------------------------------------------")
+        print ("Your password will contain special characters.")
+        return (special_characters)
+    else:
+        print ("----------------------------------------------------------------------------------")
+        print ("Your password will not contain special characters.")
 
 main ()
