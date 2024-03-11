d447e94bcb5c78a042da7c5e4b5083428dcee016
Colby <colbysmith99@hotmail.com>
2024-03-11 15:50:44 +0000

    Added password generation logic in main.py

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import random
 import string
+import time
 
+PASSWORD_LETTERS = string.ascii_letters
+PASSWORD_DIGITS = string.digits
+PASSWORD_SPECIAL = string.punctuation
     # make the user input there specified variables for the password
     # have the program define these variables and randomly generate a password from them
     # allow the user to accept this password as the one they'd like to use or let them keep generating till they're happy
 
 def main():
     print ("--------------------------------Password Generator--------------------------------")
-    define_password_length()
+    min_password_length, max_password_length = define_password_length()
+    print ("----------------------------------------------------------------------------------")     
+    contains_special_characters = define_special_characters()
     print ("----------------------------------------------------------------------------------")
-    define_special_characters()
+    contains_digits = define_digits()
     print ("----------------------------------------------------------------------------------")
-    define_digits()
-    print ("----------------------------------------------------------------------------------")
+    length = random.randint(min_password_length, max_password_length)
+    
+    if contains_special_characters and contains_digits:
+        population = PASSWORD_LETTERS + PASSWORD_DIGITS + PASSWORD_SPECIAL
+    elif contains_special_characters:
+        population = PASSWORD_LETTERS + PASSWORD_SPECIAL
+    elif contains_digits:
+        population = PASSWORD_LETTERS + PASSWORD_DIGITS
+    else:
+        population = PASSWORD_LETTERS
+
+    print(''.join(random.choice(population) for _ in range(length)))
+    
 
 def define_password_length():
     min_password_length = int(input("Please eneter the minimum amount of characters for your password, as a number: "))
@@ -21,9 +38,9 @@ def define_password_length():
     print (f"Your password will be generated between {min_password_length} and {max_password_length} characters.")
     if min_password_length > 50:
         raise Exception ("Minimum character length cannot be greater than 50")
-    if min_password_length <= 0:
+    elif min_password_length <= 0:
         raise Exception ("Minimum character length cannot be less than or equal to 0")
-    if max_password_length > 50:
+    elif max_password_length > 50:
         raise Exception ("Maximum character length cannot be greater than 100")
     if max_password_length <= min_password_length:
         raise Exception ("Maximum password length cannot be less than minimum")
@@ -35,19 +52,21 @@ def define_special_characters():
     if contains_special_characters == "yes":
         print ("----------------------------------------------------------------------------------")
         print ("Your password will contain special characters.")
-        return (contains_special_characters)
+        return True
     else:
         print ("----------------------------------------------------------------------------------")
         print ("Your password will not contain special characters.")
+        return False
 
 def define_digits():
     contains_digits = (input("Would you like your password to contain digits, yes or no: ")).lower()
     if contains_digits == "yes":
         print ("----------------------------------------------------------------------------------")
         print ("Your password will contain digits.")
-        return (contains_digits)
+        return True
     else:
         print ("----------------------------------------------------------------------------------")
         print ("Your password will not contain digits.")
+        return False
 
 main ()
